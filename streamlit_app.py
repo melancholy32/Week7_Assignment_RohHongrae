801529343b56892c5fcecd1cc95f547a8c56a8e0
Hongrae  Roh <156094157+melancholy32@users.noreply.github.com>
2024-11-03 15:49:47 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,11 +5,11 @@ from langchain.schema.output_parser import StrOutputParser
 from langchain_core.runnables.branch import RunnableBranch
 
 st.title("💬Chatbot for your flight experience")
+st.write("Please share with us your experience of the latest trip. Your feedback is really important for us.")
 feedback = st.text_area("Share with us your experience of the latest trip.")
 
 if st.button("Submit"):
   process_feedback(feedback)
-
   
 ### Get the OpenAI API key
 from google.colab import userdata
@@ -18,61 +18,64 @@ openai_api_key = userdata.get('MyOpenAIKey')
 ### Create a ChatOpenAI object
 llm = ChatOpenAI(openai_api_key=openai_api_key)
 
+
+
+
 # Show title and description.
-st.title(" Chatbot for your experience")
-st.write(
-    "Please share your experience with our flight and service. <br> Your feedback is really important for us to improve for your next journey."
+# st.title(" Chatbot for your experience")
+# st.write(
+#     "Please share your experience with our flight and service. Your feedback is really important for us."
 )
 
 #OpenAI API Key
-from langchain.chat_models import ChatOpenAI
+# from langchain.chat_models import ChatOpenAI
 
 ### Get the OpenAI API key
-from google.colab import userdata
-openai_api_key = userdata.get('MyOpenAIKey')
+# from google.colab import userdata
+# openai_api_key = userdata.get('MyOpenAIKey')
 
 ### Create a ChatOpenAI object
-chat = ChatOpenAI(openai_api_key=openai_api_key)
+# chat = ChatOpenAI(openai_api_key=openai_api_key)
 
 
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
-else:
+# if not openai_api_key:
+#     st.info("Please add your OpenAI API key to continue.", icon="🗝️")
+# else:
 
     # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
+#     client = OpenAI(api_key=openai_api_key)
 
     # Create a session state variable to store the chat messages. This ensures that the
     # messages persist across reruns.
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
+#     if "messages" not in st.session_state:
+#         st.session_state.messages = []
 
     # Display the existing chat messages via `st.chat_message`.
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
+  #   for message in st.session_state.messages:
+    #     with st.chat_message(message["role"]):
+      #       st.markdown(message["content"])
 
     # Create a chat input field to allow the user to enter a message. This will display
     # automatically at the bottom of the page.
-    if prompt := st.chat_input("What is up?"):
+#     if prompt := st.chat_input("What is up?"):
 
         # Store and display the current prompt.
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.markdown(prompt)
+#         st.session_state.messages.append({"role": "user", "content": prompt})
+#         with st.chat_message("user"):
+ #            st.markdown(prompt)
 
         # Generate a response using the OpenAI API.
-        stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
-        )
+#         stream = client.chat.completions.create(
+#             model="gpt-3.5-turbo",
+ #            messages=[
+    #             {"role": m["role"], "content": m["content"]}
+   #              for m in st.session_state.messages
+ #            ],
+  #           stream=True,
+  #       )
 
         # Stream the response to the chat using `st.write_stream`, then store it in 
         # session state.
-        with st.chat_message("assistant"):
-            response = st.write_stream(stream)
-        st.session_state.messages.append({"role": "assistant", "content": response})
+ #        with st.chat_message("assistant"):
+ #            response = st.write_stream(stream)
+ #        st.session_state.messages.append({"role": "assistant", "content": response})
