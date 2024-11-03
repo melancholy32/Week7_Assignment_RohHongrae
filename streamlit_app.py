1d7f3d4ed1e098a0ca42fb0550fafe7cdf880878
Hongrae  Roh <156094157+melancholy32@users.noreply.github.com>
2024-11-03 16:54:37 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,9 +6,6 @@ from langchain.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 from langchain.schema.output_parser import StrOutputParser
 from langchain_core.runnables.branch import RunnableBranch
-import streamlit as st
-from langchain.chat_models import ChatOpenAI
-from langchain_core.runnables.branch import RunnableBranch
 
 st.title("💬 Chatbot for your flight experience")
 st.write("Please share with us your experience of the latest trip. Your feedback is really important for us.")
